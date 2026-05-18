6b1307d1c305c3ca8fec9b88e2d27b5c71a7ebfa
Zyad-Emad <zyad.emad19@gmail.com>
2026-05-18 12:22:03 +0300

    Add project files.

---
+++ GymManagement/Controllers/PlansController.cs
@@ -0,0 +1,33 @@
+﻿using GymManagement.DbContexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace GymManagement.Controllers
+{
+    public class PlansController : Controller
+    {
+        private readonly GymDbContext _dbContext;
+        public PlansController()
+        {
+            _dbContext = new GymDbContext();
+        }
+        //Index Action
+        // GET BaseUrl/Plans/Index -> Listing ALl Plans
+        public async Task<IActionResult> Index()
+        {
+            var plans = await _dbContext.Plans.ToListAsync();
+            return View(plans);
+        }
+        //Details Action
+        // Get BaseUrl/Plans/Details/1
+        public async Task<IActionResult> Details(int id)
+        {
+            var plan = await _dbContext.Plans.FindAsync(id);
+            if (plan is null)
+                return RedirectToAction(nameof(Index));
+            else
+                return View(plan);
+        }
+    }
+}
